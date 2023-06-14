2825585f18590cccd54ae1c0cf8fe218a1daa810
Anderson Almeida <andersonvsc1998@gmail.com>
2023-06-14 15:29:11 -0300

    news wallpapers

--- fedora/install.sh
+++ fedora/install.sh
@@ -1,1 +1,2 @@
-sudo dnf install alacritty bat exa cmake zsh wget tmux neovim
+sudo dnf install alacritty bat exa cmake zsh wget tmux neovim chsh
+sudo dnf group install "C Development Tools and Libraries" "Development Tools"
